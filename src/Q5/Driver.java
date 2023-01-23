10e93b5b63777e3faa099407d24594a37d86e1ce
danny <daniel.finnegan@students.ittralee.ie>
2023-01-23 10:34:49 +0000

    Working on Password

---
+++ src/Q5/Driver.java
@@ -0,0 +1,9 @@
+package Q5;
+
+public class Driver {
+    public static void main(String[] args) {
+
+        Password P1 = new Password();
+        String Password = new P1.inputNewPassword("");
+    }
+}
---
+++ src/Q5/Password.java
@@ -0,0 +1,49 @@
+package Q5;
+
+public class Password {
+    public static String inputNewPassword (String Password)
+    {
+        boolean Length = false;
+        boolean Uppercase = false;
+        boolean Lowercase = false;
+        boolean digit = false;
+        boolean valid = true;
+        String ResultTrue = "This password is Valid";
+        String ResultFalse = "This password is not valid, Please try again";
+
+        for (int i =0;i<=Password.length();i++)
+        {
+            if (Password.length()>=8) {
+                Length = true;
+            }
+        }
+        for (int j = 0; j<=Password.length();j++)
+        {
+            if (Character.isUpperCase(Password.charAt(j)));
+            {
+                Uppercase=true;
+            }
+            if (Character.isLowerCase(Password.charAt(j)))
+            {
+                Lowercase=true;
+            }
+            if (Character.isDigit(Password.charAt(j)))
+            {
+                digit=true;
+            }
+        }
+        if (Length==true&&Uppercase==true&&Lowercase==true&&digit==true)
+        {
+            valid=true;
+        }
+        if (valid==true)
+        {
+            return ResultTrue;
+        }
+        else
+        {
+            return ResultFalse;
+        }
+
+    }
+}
